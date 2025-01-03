77547c2799e7898972009700b93d763b888281f8
Huynh Kien Hao <haohk.hkits@gmail.com>
2025-01-03 22:53:13 +0700

    design bottom navigation QR Code scanner

--- smart_canteen_fe/lib/admin/AdminScreen.dart
+++ smart_canteen_fe/lib/admin/AdminScreen.dart
@@ -1,19 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:signalr_netcore/hub_connection.dart';
-import 'package:signalr_netcore/hub_connection_builder.dart';
-import 'package:signalr_netcore/http_connection_options.dart';
-import 'package:signalr_netcore/itransport.dart';
-import '../../config_url/config.dart';
-import '../Order/order_history_admin.dart';
-import '../Category/category_screen.dart';
-import '../Order/order_screen.dart';
-import '../Product/product_screen.dart';
-import '../screen/login_screen.dart';
-import '../Profile/Profile.dart';
-import 'UserManagementAdminScreen.dart';
 import 'package:barcode_scan2/barcode_scan2.dart';
 import 'dart:convert';
+import '../Order/order_screen.dart';
+import '../Category/category_screen.dart';
+import '../Product/product_screen.dart';
+import '../Profile/Profile.dart';
+import '../Order/order_history_admin.dart';
+import 'UserManagementAdminScreen.dart';
 
 class AdminScreen extends StatefulWidget {
   @override
@@ -21,110 +14,7 @@ class AdminScreen extends StatefulWidget {
 }
 
 class _AdminScreenState extends State<AdminScreen> {
-  late HubConnection _hubConnection;
-  int _selectedIndex = 0; // Track selected index for bottom navigation.
-
-  @override
-  void initState() {
-    super.initState();
-    _initializeSignalR();
-  }
-
-  Future<void> _initializeSignalR() async {
-    _hubConnection = HubConnectionBuilder()
-        .withUrl(
-      '${Config.apiBaseUrl}/notificationHub',
-      options: HttpConnectionOptions(
-        transport: HttpTransportType.WebSockets,
-        skipNegotiation: true,
-      ),
-    )
-        .withAutomaticReconnect()
-        .build();
-
-    _hubConnection.on("NewOrderCreated", (arguments) {
-      final orderId = arguments?[0];
-      final totalPrice = arguments?[1];
-      final orderTime = arguments?[2];
-
-      _showCustomNotification(context,
-          "Đơn hàng mới: #$orderId - Tổng tiền: $totalPrice - Thời gian: $orderTime");
-    });
-
-    try {
-      await _hubConnection.start();
-      print("SignalR connection established for Admin");
-    } catch (error) {
-      print("SignalR connection error: $error");
-    }
-  }
-
-  void _showCustomNotification(BuildContext context, String message) {
-    final overlay = Overlay.of(context);
-
-    late final OverlayEntry overlayEntry;
-
-    overlayEntry = OverlayEntry(
-      builder: (context) => Positioned(
-        top: 0,
-        left: 0,
-        right: 0,
-        child: Material(
-          color: Colors.transparent,
-          child: Container(
-            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.1),
-                  blurRadius: 10,
-                  offset: Offset(0, 2),
-                ),
-              ],
-            ),
-            child: SafeArea(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Expanded(
-                    child: Text(
-                      message,
-                      style: TextStyle(color: Colors.black, fontSize: 16),
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ),
-                  IconButton(
-                    icon: Icon(Icons.close, color: Colors.black),
-                    onPressed: () {
-                      overlayEntry.remove();
-                    },
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-
-    overlay.insert(overlayEntry);
-
-    Future.delayed(Duration(seconds: 3), () {
-      overlayEntry.remove();
-    });
-  }
-
-  Future<void> _logout(BuildContext context) async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.clear();
-
-    Navigator.pushAndRemoveUntil(
-      context,
-      MaterialPageRoute(builder: (context) => LoginScreen()),
-          (route) => false,
-    );
-  }
+  int _selectedIndex = 0;
 
   Future<void> _scanQrCode(BuildContext context) async {
     try {
@@ -134,7 +24,6 @@ class _AdminScreenState extends State<AdminScreen> {
         final scannedData = result.rawContent.trim();
         print('Scanned Data: $scannedData');
 
-        // Parse the scanned data and navigate to OrderScreen.
         Navigator.push(
           context,
           MaterialPageRoute(
@@ -159,13 +48,6 @@ class _AdminScreenState extends State<AdminScreen> {
   }
 
   @override
-  void dispose() {
-    _hubConnection.stop();
-    super.dispose();
-  }
-
-  @override
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -174,49 +56,104 @@ class _AdminScreenState extends State<AdminScreen> {
           style: TextStyle(fontSize: 24, color: Colors.white),
         ),
         backgroundColor: Colors.green,
-        actions: [
-          IconButton(
-            icon: Icon(Icons.logout, color: Colors.white),
-            onPressed: () {
-              _logout(context);
-            },
-          ),
-        ],
       ),
-      body: _selectedIndex == 0
-          ? _buildMainDashboard(context)
-          : _selectedIndex == 2
-          ? ProfileScreen()
-          : _buildMainDashboard(context), // Keep default dashboard or another widget.
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.dashboard),
-            label: "Dashboard",
+      body: SafeArea(
+        child: _buildBody(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _scanQrCode(context),
+        backgroundColor: Colors.green,
+        shape: CircleBorder(),
+        child: Icon(Icons.qr_code_scanner, size: 30),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: _buildBottomNavigationBar(),
+    );
+  }
+
+  Widget _buildBody() {
+    return Column(
+      children: [
+        Expanded(
+          child: _selectedIndex == 0
+              ? _buildMainDashboard(context)
+              : ProfileScreen(),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildBottomNavigationBar() {
+    return SafeArea(
+      child: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        notchMargin: 8.0,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 3.0), // Thêm padding để tạo khoảng cách
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              // Left items
+              Row(
+                children: [
+                  _buildNavItem(
+                    context,
+                    icon: Icons.dashboard,
+                    label: "Dashboard",
+                    index: 0,
+                  ),
+                ],
+              ),
+              // Right items
+              Row(
+                children: [
+                  _buildNavItem(
+                    context,
+                    icon: Icons.person,
+                    label: "Admin",
+                    index: 2,
+                  ),
+                ],
+              ),
+            ],
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.qr_code_scanner),
-            label: "Quét mã QR",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: "Admin",
-          ),
-        ],
-        onTap: (index) async {
-          if (index == 1) {
-            await _scanQrCode(context); // Trigger QR code scanning when tapping on the second item.
-          } else {
-            setState(() {
-              _selectedIndex = index; // Update index only for other tabs.
-            });
-          }
-        },
+        ),
       ),
     );
   }
 
+  Widget _buildNavItem(
+      BuildContext context, {
+        required IconData icon,
+        required String label,
+        required int index,
+      }) {
+    final isSelected = _selectedIndex == index;
+
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _selectedIndex = index;
+        });
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 3.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(icon, color: isSelected ? Colors.green : Colors.grey),
+            Text(
+              label,
+              style: TextStyle(
+                fontSize: 12,
+                color: isSelected ? Colors.green : Colors.grey,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
   Widget _buildMainDashboard(BuildContext context) {
     return SingleChildScrollView(
@@ -235,10 +172,11 @@ class _AdminScreenState extends State<AdminScreen> {
             ),
             SizedBox(height: 20),
             GridView.count(
-              crossAxisCount: 2,
               shrinkWrap: true,
+              crossAxisCount: 2,
               crossAxisSpacing: 16,
               mainAxisSpacing: 16,
+              physics: NeverScrollableScrollPhysics(),
               children: [
                 _buildDashboardCard(
                   context,
